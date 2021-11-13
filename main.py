47b41d12af2080fd1e422761add30ed6e67bd6d1
theMathR <56732467+theMathR@users.noreply.github.com>
2021-11-13 19:34:11 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,258 @@
+from math import floor
+import pygame
+import random
+from pygame.locals import *
+from constants import *
+
+pygame.init()
+
+window = pygame.display.set_mode((TILE_NUMBER_X * TILE_SIZE, TILE_NUMBER_Y * TILE_SIZE))
+pygame.display.set_caption(TITLE)
+
+tile = pygame.Surface((TILE_SIZE, TILE_SIZE))
+mini_tile = pygame.Surface((TILE_SIZE * DUCKS, TILE_SIZE * DUCKS))
+
+turn = PLAYER1
+
+selected_tile = UNSELECTED
+
+blob_movements = {}
+
+
+def create_arrow(direction, intensity):
+    if direction in ("left", "right"):
+        arrow = pygame.Surface((TILE_SIZE * 2, TILE_SIZE))
+        pygame.draw.line(arrow, (intensity, 0, 0), (TILE_SIZE / 2, TILE_SIZE / 2), (TILE_SIZE * 1.5, TILE_SIZE / 2),
+                         ARROW_WIDTH)
+        if direction == "left":
+            pygame.draw.lines(arrow, (intensity, 0, 0), False, ((TILE_SIZE * 3 / 4, TILE_SIZE / 4),
+                                                                (TILE_SIZE / 2, TILE_SIZE / 2),
+                                                                (TILE_SIZE * 3 / 4, TILE_SIZE * 3 / 4)), ARROW_WIDTH)
+        else:
+            pygame.draw.lines(arrow, (intensity, 0, 0), False, ((TILE_SIZE * 1.25, TILE_SIZE / 4),
+                                                                (TILE_SIZE * 1.5, TILE_SIZE / 2),
+                                                                (TILE_SIZE * 1.25, TILE_SIZE * 3 / 4)), ARROW_WIDTH)
+    else:
+        arrow = pygame.Surface((TILE_SIZE, TILE_SIZE * 2))
+        pygame.draw.line(arrow, (intensity, 0, 0), (TILE_SIZE / 2, TILE_SIZE / 2), (TILE_SIZE / 2, TILE_SIZE * 1.5),
+                         ARROW_WIDTH)
+        if direction == "up":
+            pygame.draw.lines(arrow, (intensity, 0, 0), False, ((TILE_SIZE / 4, TILE_SIZE * 3 / 4),
+                                                                (TILE_SIZE / 2, TILE_SIZE / 2),
+                                                                (TILE_SIZE * 3 / 4, TILE_SIZE * 3 / 4)), ARROW_WIDTH)
+        else:
+            pygame.draw.lines(arrow, (intensity, 0, 0), False, ((TILE_SIZE / 4, TILE_SIZE * 1.25),
+                                                                (TILE_SIZE / 2, TILE_SIZE * 1.5),
+                                                                (TILE_SIZE * 3 / 4, TILE_SIZE * 1.25)), ARROW_WIDTH)
+
+    arrow.set_colorkey((0, 0, 0))
+    return arrow
+
+
+def update_blobs():
+    global blob_movements
+
+    for movement in blob_movements.keys():
+        grid[movement[0][0]][movement[0][1]][1] -= blob_movements[movement] + MOVEMENT_COST
+        if grid[movement[0][0]][movement[0][1]][1] == GROUND:
+            grid[movement[0][0]][movement[0][1]][0] = 0
+
+        grid[movement[1][0]][movement[1][1]][1] += blob_movements[movement]
+        grid[movement[1][0]][movement[1][1]][0] = turn
+
+        if len(grid[movement[1][0]][movement[1][1]]) > 2:
+            del grid[movement[1][0]][movement[1][1]][2]
+
+    blob_movements = {}
+
+
+def update_bacterias_mushrooms():
+    for x in range(TILE_NUMBER_X):
+        for y in range(TILE_NUMBER_Y):
+            if grid[x][y][0] == BACTERIA:
+                grid[x][y][1] += BACTERIA_GROWTH
+
+            elif grid[x][y][0] == MUSHROOM:
+                grid[x][y][1] += MUSHROOM_GROWTH
+
+    for x in range(TILE_NUMBER_X):
+        for y in range(TILE_NUMBER_Y):
+            if grid[x][y][0] == BACTERIA and grid[x][y][1] > BACTERIA_REPRODUCTION_THRESHOLD:
+                for spawn in (
+                        (x - 1, y),
+                        (x + 1, y),
+                        (x, y - 1),
+                        (x, y + 1)
+                ):
+                    if 0 <= spawn[0] <= TILE_NUMBER_X - 1 and 0 <= spawn[1] <= TILE_NUMBER_Y - 1:
+                        if grid[spawn[0]][spawn[1]][0] == GROUND:
+                            grid[spawn[0]][spawn[1]][0] = BACTERIA
+                            grid[spawn[0]][spawn[1]][1] = BACTERIA_SPAWN_VALUE
+
+            if grid[x][y][0] == MUSHROOM and grid[x][y][1] > MUSHROOM_REPRODUCTION_THRESHOLD and not grid[x][y][2]:
+                for spawn in (
+                        (random.randint(0, TILE_NUMBER_X - 1), random.randint(0, TILE_NUMBER_Y - 1))
+                        for _ in range(MUSHROOM_SPORE_NUMBER)
+                ):
+                    if grid[spawn[0]][spawn[1]][0] == GROUND:
+                        grid[spawn[0]][spawn[1]][0] = MUSHROOM
+                        grid[spawn[0]][spawn[1]][1] = MUSHROOM_SPAWN_VALUE
+                        grid[spawn[0]][spawn[1]].append(False)
+                grid[x][y][2] = True
+
+
+def update_screen():
+    for x in range(TILE_NUMBER_X):
+        for y in range(TILE_NUMBER_Y):
+            color = GROUND_COLOR
+
+            if grid[x][y][1] > MAX_VALUE:
+                grid[x][y][1] = MAX_VALUE
+
+            intensity = int(grid[x][y][1] * 127 / MAX_VALUE + 127)
+
+            if grid[x][y][0] == PLAYER1:
+                color = (intensity, intensity, 0)
+            elif grid[x][y][0] == PLAYER2:
+                color = (intensity, floor(intensity / 2), 0)
+            elif grid[x][y][0] == BACTERIA:
+                color = (0, intensity, 0)
+            elif grid[x][y][0] == MUSHROOM:
+                color = (0, 0, intensity)
+
+            if selected_tile == (x, y):
+                tile.fill(SELECTED_TILE_COLOR)
+                mini_tile.fill(color)
+                tile.blit(mini_tile, ((TILE_SIZE - DUCKS * TILE_SIZE) / 2, (TILE_SIZE - DUCKS * TILE_SIZE) / 2))
+
+            elif selected_tile in (
+                    (x + 1, y),
+                    (x - 1, y),
+                    (x, y + 1),
+                    (x, y - 1)
+            ):
+                tile.fill(DESTINATION_TILE_COLOR)
+                mini_tile.fill(color)
+                tile.blit(mini_tile, ((TILE_SIZE - DUCKS * TILE_SIZE) / 2, (TILE_SIZE - DUCKS * TILE_SIZE) / 2))
+
+            else:
+                tile.fill(color)
+
+            window.blit(tile, (x * TILE_SIZE, y * TILE_SIZE))
+
+    for movement in blob_movements.keys():
+        intensity = int(blob_movements[movement] * 127 / (grid[movement[0][0]][movement[0][1]][1] + 1) + 127)
+
+        if movement[0][0] > movement[1][0]:
+            arrow = create_arrow("left", intensity)
+        elif movement[0][0] < movement[1][0]:
+            arrow = create_arrow("right", intensity)
+        elif movement[0][1] > movement[1][1]:
+            arrow = create_arrow("up", intensity)
+        elif movement[0][1] < movement[1][1]:
+            arrow = create_arrow("down", intensity)
+
+        window.blit(arrow, (min(movement[0][0], movement[1][0]) * TILE_SIZE,
+                            min(movement[0][1], movement[1][1]) * TILE_SIZE))
+
+    pygame.display.update()
+
+
+def move_blob(from_, to):
+    if (to, from_) in blob_movements.keys():
+        blob_movements[(to, from_)] -= 1
+        if blob_movements[(to, from_)] == 0:
+            del blob_movements[(to, from_)]
+        return
+
+    if (from_, to) in blob_movements.keys():
+        if blob_movements[(from_, to)] + MOVEMENT_COST < grid[from_[0]][from_[1]][1]:
+            blob_movements[(from_, to)] += 1
+        else:
+            del blob_movements[(from_, to)]
+        return
+
+    blob_movements[(from_, to)] = 1
+
+
+def check_game_won():
+    player1_value = 0
+    player2_value = 0
+    still_food = False
+
+    for x in range(TILE_NUMBER_X):
+        for y in range(TILE_NUMBER_Y):
+            if grid[x][y][0] == 1:
+                player1_value += grid[x][y][1]
+            elif grid[x][y][0] == 2:
+                player2_value += grid[x][y][1]
+            else:
+                still_food = still_food or grid[x][y][0] in (3, 4)
+
+    if not still_food:
+        pygame.quit()
+        if player1_value > player2_value:
+            print("Player 1 won!")
+        elif player2_value > player1_value:
+            print("Player 2 won!")
+        else:
+            print("No one won!")
+        input()
+        quit()
+
+    if player1_value == 0:
+        pygame.quit()
+        print("Player 2 won!")
+        input()
+        quit()
+
+    if player2_value == 0:
+        pygame.quit()
+        print("Player 1 won!")
+        input()
+        quit()
+
+
+update_screen()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            quit()
+
+        elif event.type == MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]:
+                clicked_tile = (
+                    floor(pygame.mouse.get_pos()[0] / TILE_SIZE), floor(pygame.mouse.get_pos()[1] / TILE_SIZE))
+
+                if clicked_tile in (
+                        (selected_tile[0] + 1, selected_tile[1]),
+                        (selected_tile[0] - 1, selected_tile[1]),
+                        (selected_tile[0], selected_tile[1] + 1),
+                        (selected_tile[0], selected_tile[1] - 1)
+                ):
+                    move_blob(selected_tile, clicked_tile)
+
+                    selected_tile = UNSELECTED
+
+                elif grid[clicked_tile[0]][clicked_tile[1]][0] == turn:
+                    selected_tile = clicked_tile
+
+                else:
+                    selected_tile = UNSELECTED
+
+                update_screen()
+
+        elif event.type == KEYDOWN:
+            if event.key == K_RETURN:
+                selected_tile = UNSELECTED
+
+                update_blobs()
+                update_bacterias_mushrooms()
+                check_game_won()
+                update_screen()
+
+                turn += 1
+                if turn == 3:
+                    turn = 1
